3c90dd2a21583b171c9b7eb8a149b33beaa15836
Anurag Yadav <ay177557@gmail.com>
2024-05-26 00:11:11 +0530

    Create Question6.c

---
+++ Question6.c
@@ -0,0 +1,11 @@
+// ANURAG YADAV
+// ROLL NO 2303510100024
+#include <stdio.h>
+int main() {
+   int a,b,c;
+   printf("Enter three number: ");
+   scanf("%d %d %d",&a,&b,&c);
+   float sum=a+b+c;
+   printf("average is %f",sum/3);
+    return 0;
+}
