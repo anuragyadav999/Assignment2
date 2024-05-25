75e5591a42a0eef0ec070b5dd805b0fe58139fad
Anurag Yadav <ay177557@gmail.com>
2024-05-26 00:12:46 +0530

    Create Question10.c

---
+++ Question10.c
@@ -0,0 +1,15 @@
+// ANURAG YADAV
+// 2303510100024
+#include <stdio.h>
+int main() {
+    char a,d,c;
+    printf("Enter the three characters: ");
+    scanf("%c %c %c",&a,&d,&c);
+    int p=a;
+    int q=d;
+    int r=c;
+    printf("%c----%d\n",a,p);
+    printf("%c----%d\n",d,q);
+    printf("%c----%d\n",c,r);
+    return 0;
+}
