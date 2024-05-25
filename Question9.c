1934878eb88a8f1b2968d21d3025f5ee37995887
Anurag Yadav <ay177557@gmail.com>
2024-05-26 00:12:21 +0530

    Create Question9.c

---
+++ Question9.c
@@ -0,0 +1,14 @@
+// ANURAG YADAV
+// 2303510100024
+#include <stdio.h>
+int main() {
+    int l,b,h;
+    printf("Enter the length of cuboid: ");
+    scanf("%d",&l);
+    printf("Enter the breadth of cuboid: ");
+    scanf("%d",&b);
+    printf("Enter the height of cuboid: ");
+    scanf("%d",&h);
+    printf("the volume of the cuboid id %d",l*b*h);
+    return 0;
+}
