1e9af1070ff76b8527254fc8745ced2f17940aa2
Anurag Yadav <ay177557@gmail.com>
2024-05-26 00:10:18 +0530

    Create Question4.c

---
+++ Question4.c
@@ -0,0 +1,18 @@
+// ANURAG YADAV
+// ROLL NO 2303510100024
+#include <stdio.h>
+int main() {
+   int a,b,c;
+   printf("Enter the any number: ");
+   scanf("%d ",&a);
+   if(a>0){
+       printf("positive");
+   }
+   else if(a<0){
+       printf("negative");
+   }
+   else{
+       printf("zero");
+   }
+    return 0;
+}
