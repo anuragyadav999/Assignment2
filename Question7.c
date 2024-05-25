783c3111a4040fbb0af46f0eb8ab583cbf6b35a6
Anurag Yadav <ay177557@gmail.com>
2024-05-26 00:11:34 +0530

    Create Question7.c

---
+++ Question7.c
@@ -0,0 +1,10 @@
+// ANURAG YADAV
+// ROLL NO 2303510100024
+#include <stdio.h>
+int main() {
+   float a;
+   printf("Enter the radius of the circle: ");
+   scanf("%f",&a);
+   printf("The circumference of the circle is %f",2*3.14*a);
+    return 0;
+}
