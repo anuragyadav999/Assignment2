21ac450f417a00bdbbf5eca41070d68262229dbe
Anurag Yadav <ay177557@gmail.com>
2024-05-26 00:13:59 +0530

    Create Question12.c

---
+++ Question12.c
@@ -0,0 +1,29 @@
+// ANURAG YADAV
+// 2303510100024
+#include <stdio.h>
+int main() {
+    int marks;
+    printf("Enter your marks: ");
+    scanf("%d",&marks);
+    if(marks>=91 && age<=100){
+        printf("A")
+    }
+    else if(marks>=81 && marks<=90){
+        printf("B");
+    }
+    else if(marks>=71 && marks<=80){
+        printf("C");
+    }
+    else if(marks>=61 && marks<=70){
+        printf("D");
+    }
+    else if(marks>=51 && marks<=60){
+        printf("E");
+    }
+    else if(marks>=0 && marks<=50){
+        printf("F");
+    }
+    else{
+        printf("Wrong entry");
+    }
+}
