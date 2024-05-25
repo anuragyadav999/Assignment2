6112eac74248d2d44c2465a4a452da48eb0fff7d
Anurag Yadav <ay177557@gmail.com>
2024-05-26 00:08:45 +0530

    Create Question1.c

---
+++ Question1.c
@@ -0,0 +1,15 @@
+// ANURAG YADAV
+// ROLL NO 2303510100024
+#include <stdio.h>
+int main() {
+   int a;
+   printf("Enter the year: ");
+   scanf("%d",&a);
+   if(a%4==0){
+       printf("Given year is a leap year");
+   }
+   else{
+       printf("Given year is not a leap year");
+   }
+    return 0;
+}
